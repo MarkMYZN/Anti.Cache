f40cbd7088fb24049279082d54fc0897a11f76e0
Mark <141608680+MarkMYZN@users.noreply.github.com>
2023-11-15 13:43:45 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,14 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+char ComputerName = 
+
+int main(){
+    
+    system("echo off");
+    
+  printf("Computer name : %s")
+
+return 0;
+}
