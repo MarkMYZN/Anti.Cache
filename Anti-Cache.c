99c1e90c91563f10cefa733a9d4388ebc5c72d44
Mark <141608680+MarkMYZN@users.noreply.github.com>
2023-12-11 22:29:40 +0800

    Update Anti-Cache.c
    I created this software with the idea of helping people with slow computers.
            
                If ran, it will optimize, clean, refresh, and repair missing or corrupted files
            in your computer.
            
            Make sure to run it as Administrator, And please proceed with caution.

--- Anti-Cache.c
+++ Anti-Cache.c
@@ -4,23 +4,74 @@
 
 int main(){
     
+    /*
+        I created this software with the idea of helping people with slow computers.
+        
+            If ran, it will optimize, clean, refresh, and repair missing or corrupted files
+        in your computer.
+        
+        Make sure to run it as Administrator, And please proceed with caution.
+    */
+
+    system("echo Created By MarkMYZN");
     system("echo off");
 
-    // ==========|| Browser Cache Deletion ||==========
-    system("del C:\\Users\\Miyazono\\AppData\\Roaming\\discord\\Cache /q");
-    system("del C:\\Users\\Miyazono\\AppData\\Local\\Steam\\htmlcache\\Cache /q");
-    system("del C:\\Users\\Miyazono\\AppData\\Local\\Steam\\htmlcache\\GPUCache /q");
+    system("wmic.exe /Namespace:\\\root\\default Path SystemRestore Call CreateRestorePoint "Latest Restore-Point", 100, 7");
 
-    // ==========|| App Cache Deletion ||==========
-    system("del C:\\Users\\Miyazono\\AppData\\Roaming\\discord\\Cache /q");
-    system("del C:\\Users\\Miyazono\\AppData\\Local\\Steam\\htmlcache\\GPUCache /q");
-    system("del C:\\Users\\Miyazono\\AppData\\Local\\Steam\\htmlcache\\Cache /q");
-    system("del C:\\Windows\\Prefetch /f /q");
+    system("powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61"); 
+    // doesnt do anything more than add an additional power "ultimare" power feature
 
-    // ==========|| Temporary File Remover ||==========
+    // ==========|| Disables Some apps from Starting in Logon ||==========
+    system("reg add HKLM\\SOFTWARE\\Microsoft\\Active Setup\\Installed Components /v Google Chrome /t REG_SZ /d "" /f");
+    system("reg add HKLM\\SOFTWARE\\Microsoft\\Active Setup\\Installed Components /v Microsoft Edge /t REG_SZ /d "" /f");
+    system("reg add HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v Steam /t REG_SZ /d "" /f");
+    system("reg add HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v MicrosoftEdgeAutoLaunch_F0F41C1CE79B46820675944F246DE300 /t REG_SZ /d "" /f");
+    system("reg add HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v Discord /t REG_SZ /d "" /f");
+
+    // ==========|| Disable HPET ||==========
+    system("bcdedit /deletevalue useplatformclock");
+    system("bcdedit /set disabledynamictick yes");
+    system("bcdedit /set useplatformtick yes");
+
+    // ==========|| System Junk Cleanup ||==========
+    system("del %%temp%% /f /s /q");
+    system("del %%Windir%%\\Temp /f /s /q");
+    system("del C:\\WINDOWS\\Prefetch  /f /s /q");
+    system("RD /S /Q C:\\$Recycle.Bin");
+    system("RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8");
+    system("RD /S /Q C:\\Users\\YourUsername\\AppData\\Local\\Microsoft\\Windows\\Explorer");
+    system("net stop wuauserv");
+    system("RD /S /Q C:\\Windows\\SoftwareDistribution");
+    system("net start wuauserv");
+
+    // ==========|| Application Junk Cleanup ||==========
     system("del C:\\Windows\\Temp /f /q");
     system("del C:\\Users\\Miyazono\\AppData\\Local\\D3DSCache /f /q");
     system("del C:\\Users\\Miyazono\\AppData\\Local\\Microsoft\\Windows\\Explorer /f /q");
+
+    // ==========|| AMD Optimizer (CPU and GPU) ||========== (this interacts with the registry editor.)
+
+    //AMD CPU Priority - High
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Multimedia\\SystemProfile\\Tasks\\Games\" /v \"GPU Priority\" /t REG_DWORD /d 8 /f");
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Multimedia\\SystemProfile\\Tasks\\Games\" /v \"Priority\" /t REG_DWORD /d 6 /f");
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Multimedia\\SystemProfile\\Tasks\\Games\" /v \"Scheduling Category\" /t REG_SZ /d \"High\" /f");
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Multimedia\\SystemProfile\\Tasks\\Games\" /v \"SFIO Priority\" /t REG_SZ /d \"High\" /f");
+
+    //AMD GPU Priority - High
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Multimedia\\SystemProfile\\Tasks\\Games\" /v \"GPU Priority\" /t REG_DWORD /d 8 /f");
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Multimedia\\SystemProfile\\Tasks\\Games\" /v \"Priority\" /t REG_DWORD /d 6 /f");
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Multimedia\\SystemProfile\\Tasks\\Games\" /v \"Scheduling Category\" /t REG_SZ /d \"High\" /f");
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Multimedia\\SystemProfile\\Tasks\\Games\" /v \"SFIO Priority\" /t REG_SZ /d \"High\" /f");
+
+    //Boost System Responsiveness
+    system("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Multimedia\\SystemProfile\" /v \"SystemResponsiveness\" /t REG_DWORD /d 1 /f");
+
+    //Disable GPU Energy Driver
+    system("reg add \"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\GpuEnergyDrv\" /v \"Start\" /t REG_DWORD /d 4 /f");
+
+    //Disable GPU Power Throttling
+    system("reg add \"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Power\\PowerThrottling\" /v \"PowerThrottlingOff\" /t REG_DWORD /d 1 /f");
+
 
     // ==========|| IP Flushing and Renewal ||==========
     system("ipconfig /flushdns");
@@ -42,8 +93,13 @@ int main(){
     // PC Shutdown
     system("shutdown -s -t 0");
 
+    // PC Restart (change if needed)
+    // system("shutdown -r -t 0");
+
+
+    //                    ==========|| Other commands: ||==========
     /*
-    ==========|| Other commands: ||==========
+    ==========|| These are some commands that didnt go with the main code. ||==========
 
     //ipconfig /flushdns
     //ipconfig /registerdns
@@ -59,6 +115,11 @@ int main(){
     //DISM /Online /Cleanup-Image /ScanHealth
     //DISM /Online /Cleanup-Image /RestoreHealth /Source:repairSource\install.wim
     
+    // ==========|| App Cache Deletion ||==========
+    system("del C:\\Users\\Miyazono\\AppData\\Roaming\\discord\\Cache /q");
+    system("del C:\\Users\\Miyazono\\AppData\\Local\\Steam\\htmlcache\\GPUCache /q");
+    system("del C:\\Users\\Miyazono\\AppData\\Local\\Steam\\htmlcache\\Cache /q");
+    system("del C:\\Windows\\Prefetch /f /q");
     */
 
 return 0;
